f2300190584924ce74bd0f7b90ec1b7edcc14981
Chiza Barber <chiza.alba@gmail.com>
2019-02-21 16:15:22 -0600

    tweak googlestrategy settings

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,8 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true // fixes http(s) proxy issue
     }, (accessToken, refreshToken, profile, done) => {
             User.findOne({ googleId: profile.id })
                 .then((existingUser) => {
